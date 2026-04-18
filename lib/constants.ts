57971af6b3e91ffdf05ead94f344de8b0bf9f779
Vincent <bondiclevincent@gmail.com>
2026-04-18 12:19:37 +0100

    first commit

---
+++ lib/constants.ts
@@ -0,0 +1,36 @@
+import { Props as EventCardProps } from "@/components/EventCard";
+
+export const events: EventCardProps[] = [
+  {
+    title: "Next.js Conf 2024",
+    image: "/images/event1.png",
+    slug: "nextjs-conf-2024",
+    location: "San Francisco, CA & Online",
+    date: "October 22-23, 2024",
+    time: "9:00 AM - 5:00 PM PST",
+  },
+  {
+    title: "React Summit 2024",
+    image: "/images/event2.png",
+    slug: "react-summit-2024",
+    location: "Amsterdam, Netherlands & Online",
+    date: "June 14-15, 2024",
+    time: "10:00 AM - 6:00 PM CEST",
+  },
+  {
+    title: "Open Source Hackathon",
+    image: "/images/event3.png",
+    slug: "open-source-hackathon-2024",
+    location: "Virtual",
+    date: "November 8-10, 2024",
+    time: "Starts 6:00 PM UTC",
+  },
+  {
+    title: "Local Dev Meetup: AI in Web Dev",
+    image: "/images/event1.png",
+    slug: "local-ai-meetup",
+    location: "Tech Hub, London",
+    date: "September 5, 2024",
+    time: "6:30 PM - 8:30 PM BST",
+  },
+];
